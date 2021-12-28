2c44c5135c795d0713de360afa54c2da642a5ff3
sania-jay <92186934+sania-jay@users.noreply.github.com>
2021-12-28 14:46:56 +0000

    add search form and city with weather as well as icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
 import Weather from "./Weather";
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <h1>
-          Hello World!!!
-        </h1>
-       <Weather />
+      
+        <Weather />
       </header>
     </div>
   );
